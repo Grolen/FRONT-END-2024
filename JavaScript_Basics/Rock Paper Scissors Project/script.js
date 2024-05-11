12af283547ad3128d3ded4ee41b0c69d334bbc78
Grolen <pein091@gmail.com>
2024-05-11 13:30:35 -0400

    making a new project, wrote steps and created blank html/css/js files

---
+++ JavaScript_Basics/Rock Paper Scissors Project/script.js
@@ -0,0 +1,17 @@
+//TODO: Rock Paper Scissors game!
+//STEPS
+// 1. Create a new function named getComputerChoise
+// 2. Write the code so that getComputerChoise will randomly return one of the following string values: "rock", "paper" or "scissors" (using Math.random())
+// 3. Test that your function returns what you expect using console.log or the browser developer tools before advancing to the next step
+// 4. Create a new function named getHumanChoise 
+// 5. Write the code so that getHumanChoise will return one of the valid chioces depending on what the user inputs
+// 6. Test what your function returns by using console.log
+// 7. Create two new variables named humanScore and computerScore in the global scope
+// 8. Initialize those variables with the value of 0
+// 9. Create a new function named playRound
+// 10. Define two params for playRound: humanChoice and computerChoice. Use there two params to take the human and computer chioces as args
+// 11. Make your funcion's humanSelecion param case-insensitive so that players can input "rock", "ROCK", etc.
+// 12. Whire the code for your playRound function to console.log a string value representing the round winner such as "You lose! Paper beats Rock!"
+// 13. Increment the humanScore or computerScore variable based in the round winner
+//* LETS START!!!
+
