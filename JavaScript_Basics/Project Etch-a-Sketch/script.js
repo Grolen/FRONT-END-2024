bd5fd815c0695e6821128ef5655f4bce368dead7
Grolen <pein091@gmail.com>
2024-05-17 09:34:40 -0400

    added UI to enter users number and built a grid number*number

--- JavaScript_Basics/Project Etch-a-Sketch/script.js
+++ JavaScript_Basics/Project Etch-a-Sketch/script.js
@@ -1,10 +1,30 @@
+let divsAmount = 256;
+
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
 
-const divsAmount = 256;
+button.addEventListener("click", submitAnAmount)
 
-for (let i = 1; i <= divsAmount; i++) {
-      const div = document.createElement('div');
-      div.classList.add("div-child")
-      div.textContent = `${i}`;
-      container.appendChild(div);
+function submitAnAmount() {
+    container.innerHTML = '';
+    divsAmount = parseInt(prompt("Enter a number of a grid (max 100): "))
+    if(Number.isInteger(divsAmount) && divsAmount <= 100) {
+        generateGrid(divsAmount)
+    } else { 
+        alert("Enter a valid number")
+    }
+}
+
+function generateGrid(usersNumber) {
+    for (let i = 1; i <= usersNumber; i++) {
+        const divColumn = document.createElement('div');
+        divColumn.classList.add("column")
+        container.appendChild(divColumn);
+        for( let j = 1; j <= usersNumber; j++) {
+            const divRow = document.createElement('div');
+            divRow.classList.add("row");
+            divRow.textContent = `div`;
+            divColumn.appendChild(divRow)
+        }
+  }
 }
