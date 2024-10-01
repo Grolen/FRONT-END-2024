a4487976fef98bb1ef5cf70c65b2f1167665636a
Vladyslav Shyrokov <vladyslav.shyrokov98@gmail.com>
2024-10-01 12:26:04 -0400

    was working about namings because I'm gonna exted the folders so I'm giving them appropriate names

--- JavaScript_Course_by_Odin/Porject Library/script.js
+++ JavaScript_Course_by_Odin/Porject Library/script.js
@@ -19,4 +19,18 @@ console.log(Object.getPrototypeOf(someBook) === Book.prototype)
 
 // *Project: Library
 
+const myLibrary = ['Book one, Author Me, 240', 'Book two, Author Friend, 120', 'Book three, Author Enemy, 5'];
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.info = function () {
+        return title + ' ' + author + ' ' + pages;
+    }
+}
+
+function addBookToLibrary() {
+    
+}
 
