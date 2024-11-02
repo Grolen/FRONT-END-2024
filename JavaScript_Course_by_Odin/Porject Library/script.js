470875615a703bba00614deed647a645b87a4b0b
Grolen <vladyslav.shyrokov98@gmail.com>
2024-11-02 15:22:25 -0400

    started working on Odin's Library Project

--- JavaScript_Course_by_Odin/Porject Library/script.js
+++ JavaScript_Course_by_Odin/Porject Library/script.js
@@ -1,42 +1,80 @@
-// *Objects and prototypes
-
-function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.info = function () {
-        return title + ' ' + author + ' ' + pages;
-    }
-}
-
-const someBook = new Book ('Po lby', 'styk', 24)
-
-console.log(someBook.info())
-
-// ! What is that and why the result is true??
-
-console.log(Object.getPrototypeOf(someBook) === Book.prototype)
-
 // *Project: Library
 
-const myLibrary = ['Book one, Author Me, 240', 'Book two, Author Friend, 120', 'Book three, Author Enemy, 5'];
+const body = document.querySelector("body");
+const library = document.querySelector(".library");
+
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+const myLibrary = [
+  { title: "Book one", author: "Me", pages: "228" },
+  { title: "Book two", author: "Me", pages: "228" },
+  { title: "Book three", author: "Me", pages: "228" },
+];
+
+function creatingLibraryDOM() {
+  myLibrary.forEach((book) => {
+    const title = book.title;
+    const author = book.author;
+    const pages = book.pages;
+    const parentEl = document.createElement("div");
+    parentEl.className = "book";
+    parentEl.innerHTML = `
+        <h3 class="book-title">${title}</h3>
+        <p class="book-author">${author}</p>
+        <p class="book-pages">${pages}</p>
+        `;
+    library.appendChild(parentEl);
+  });
+}
+
+creatingLibraryDOM();
+
+function updatingLibraryDOM() {
+  const addedBook = myLibrary[myLibrary.length - 1];
+  const title = addedBook.title;
+  const author = addedBook.author;
+  const pages = addedBook.pages;
+  const parentEl = document.createElement("div");
+  parentEl.className = "book";
+  parentEl.innerHTML = `
+      <h3 class="book-title">${title}</h3>
+      <p class="book-author">${author}</p>
+      <p class="book-pages">${pages}</p>
+      `;
+  library.appendChild(parentEl);
+}
 
 function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.info = function () {
-        return title + ' ' + author + ' ' + pages;
-    }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.info = function () {
+    return title + " " + author + " " + pages;
+  };
 }
 
 function addBookToLibrary() {
-    const bookTitle = prompt("Enter the book's title!")
-    const bookAuthor = prompt("Enter the book's author!")
-    const bookPages = prompt("Enter the book's number of pages!")
-    const userNewBook = new Book(bookTitle, bookAuthor, bookPages)
-    const objToString = userNewBook.title + ', ' + userNewBook.author + ', ' + userNewBook.pages
-    console.log(objToString)
+  if (
+    inputs[0].value !== "" &&
+    inputs[1].value !== "" &&
+    inputs[2].value !== ""
+  ) {
+    const bookTitle = inputs[0].value;
+    const bookAuthor = inputs[1].value;
+    const bookPages = inputs[2].value;
+    const newBook = new Book(bookTitle, bookAuthor, bookPages);
+    myLibrary.push(newBook);
+    updatingLibraryDOM();
+    inputs[0].value = "";
+    inputs[1].value = "";
+    inputs[2].value = "";
+  }
 }
 
-//! TESTING SOURCE CONTROL ON MY NEW LAPTOP
+const addBookButton = document.querySelector(".submit-form");
+
+addBookButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+});
