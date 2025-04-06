0baa6aebdf81f7ba8baa01359a06783567d7b345
Grolen <vladyslav.shyrokov98@gmail.com>
2025-04-06 08:33:23 -0300

    IM BACK! created a new folder for a new project

---
+++ HTML_CSS_Basics/freeCodeCamp/Palindrome Checker Project/script.js
@@ -0,0 +1,9 @@
+// TODO: I need to write a palindrome checker
+// *step 1: pull out the input and button from HTML
+// *step 2: create an event listener that will process a value from the input 
+// *step 3: inside the listener I need:
+// a. remove all the dots and spaces in the value (using regex or alt)
+// b. .split("") the value and make it an array
+// c. create a func length checker (odd or even), using "if" statement
+// d. write code for both cases. if even - divide the array in half, if odd - use the element thats on the middle and double it (ex: "abcba" - "c" in the middle so I need to make it read for my code as "abccba")
+// e. show to a user the result of his value he did put (true or false).
